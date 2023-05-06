e36f1dceb8cf919b2d4b7e68ad1e1300edaee87d
zenongithub <mlutfizeina@gmail.com>
2023-05-06 21:11:37 +0700

    SUDAH PUSING LANJOT BESOK

--- app/src/main/java/com/shedenk/app/ui/beranda/BerandaFragment.java
+++ app/src/main/java/com/shedenk/app/ui/beranda/BerandaFragment.java
@@ -2,21 +2,36 @@ package com.shedenk.app.ui.beranda;
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.shedenk.app.R;
 import com.shedenk.app.databinding.FragmentBerandaBinding;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+import com.smarteist.autoimageslider.SliderViewAdapter;
 
 public class BerandaFragment extends Fragment {
 
+    SliderView sliderView;
+    int[] images = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3, R.drawable.slide4,};
     private FragmentBerandaBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_beranda,container,false);
 
+        sliderView = (sliderView).findViewById(R.id.image_slider);
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
+        return inflater.inflate(R.layout.fragment_beranda,container,false);
     }
 
     @Override
@@ -24,4 +39,5 @@ public class BerandaFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
---
+++ app/src/main/java/com/shedenk/app/ui/beranda/SliderAdapter.java
@@ -0,0 +1,48 @@
+package com.shedenk.app.ui.beranda;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.shedenk.app.R;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+public class SliderAdapter extends SliderViewAdapter<SliderAdapter.ViewHolder> {
+
+    int[] images;
+
+    public SliderAdapter(int[] images){
+        this.images = images;
+    }
+
+    @Override
+    public SliderAdapter.ViewHolder onCreateViewHolder(ViewGroup parent) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.slider_item_beranda,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+
+        viewHolder.imageView.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    public class ViewHolder extends SliderViewAdapter.ViewHolder{
+
+        ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+        imageView = itemView.findViewById(R.id.image_view);
+        }
+    }
+}
