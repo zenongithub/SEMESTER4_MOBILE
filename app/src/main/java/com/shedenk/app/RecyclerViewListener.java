8b70671348bc2224dcd670960da04ddc99fcf3dc
zenongithub <mlutfizeina@gmail.com>
2023-05-29 05:24:17 +0700

    Finishing

--- app/src/main/java/com/shedenk/app/RecyclerViewListener.java
+++
@@ -1,8 +0,0 @@
-package com.shedenk.app;
-
-import android.view.View;
-
-public interface RecyclerViewListener {
-    public void onClickItem(View view, int position);
-    public void onClickHapusSimpan(View view, int position);
-}
